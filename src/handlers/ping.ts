9f85d7f3904b3c2c728d18c04d0395dcfb35d8d4
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-21 09:38:40 +0700

    test: removed testing stuffs

--- src/handlers/ping.ts
+++ src/handlers/ping.ts
@@ -1,11 +1,11 @@
 import { HandlerFunction } from "@octokit/webhooks/dist-types/types";
 import { Context } from "telegraf";
 
-export const ping = 
-  (ctx: Context): HandlerFunction<"ping", unknown> => 
+export const ping =
+  (ctx: Context): HandlerFunction<"ping", unknown> =>
   async (event) => {
     await ctx.telegram.sendMessage(
-      ctx.chat?.id ?? String(process.env.HOME_ID ?? ""), 
+      ctx.chat?.id ?? String(process.env.HOME_ID ?? ""),
       `You got pinged! ${event.payload.zen}`
     )
 }
