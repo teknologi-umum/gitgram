33cc95977bbe3fc973d665af87d24f42742d24d5
arnoldart <adoni.091203@gmail.com>
2021-12-22 19:58:32 +0700

    chore: implement deployment status

--- src/handlers/deployment.ts
+++ src/handlers/deployment.ts
@@ -1,8 +1,19 @@
 import type { HandlerFunction } from "@octokit/webhooks/dist-types/types";
 import type { Context } from "telegraf";
+import templite from "templite";
 
 export const deploymentStatus = 
   (ctx: Context): HandlerFunction<"deployment_status", unknown> => 
     async (event) => {
-      //
+      const template = `♻️ Deployment Status
+        <b>Status</b> {{status}}
+      `;
+
+      const response = templite(template, {
+        status: event.payload.deployment_status.state
+      });
+      
+      ctx.chat?.id ?? String(process.env.HOME_ID ?? ""),
+      response,
+      { parse_mode: "HTML", disable_web_page_preview: true };
     };
