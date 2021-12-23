8c0712fdb6252bf4e8c975577933321706263d37
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-23 23:35:07 +0700

    chore: clearer release message

--- src/handlers/release.ts
+++ src/handlers/release.ts
@@ -3,20 +3,23 @@ import type { Context } from "telegraf";
 import templite from "templite";
 import { markdownToHTML } from "../utils/markdown";
 
-// start dulu, baru kirim event
-
 export function release(
   ctx: Context
 ): HandlerFunction<"release.published", unknown> {
   return async (event) => {
     const template = `<b>🌱 New Release for <a href="https://github.com/{{repoName}}>{{repoName}}</a></b>
 
-  {{body}}
+<a href="{{url}}">{{name}}</a>
 
-  <b>Tag Name:</b> {{tag_name}}`;
+{{body}}
+
+<b>Tag Name:</b> {{tag_name}}`;
 
     const response = templite(template, {
       tag_name: event.payload.release.tag_name,
+      repoName: event.payload.repository.name,
+      name: event.payload.release.name,
+      url: event.payload.release.url,
       body: markdownToHTML(event.payload.release.body) || "<i>No description provided.</i>"
     });
 
