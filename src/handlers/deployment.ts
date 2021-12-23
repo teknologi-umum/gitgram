5a5828d37bcd899b15b849c6e8ac9fe397c067c1
arnoldart <adoni.091203@gmail.com>
2021-12-23 15:58:02 +0700

    chore: solve white space in deployment template

--- src/handlers/deployment.ts
+++ src/handlers/deployment.ts
@@ -7,9 +7,9 @@ export const deploymentStatus =
     async (event) => {
       const template = `♻️ Deployment Status:
         <b>Repo Name: </b> <a href="https://github.com/{{repoName}}">{{repoName}}</a>
-        <b>Status: </b> {{status}}
-        <b>Target URL: </b> {{targetUrl}}
-        <b>Description: </b> {{description}}`;
+        <b>Status:</b> {{status}}
+        <b>Target URL:</b> {{targetUrl}}
+        <b>Description:</b> {{description}}`;
 
       const response = templite(template, {
         repoName: event.payload.repository.full_name,
