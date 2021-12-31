0ef7a4d6f216b3eb2b66d32b674fb5af4c9a7098
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-31 14:49:46 +0700

    fix: trim html function
    stolen from teknologi umum bot

---
+++ src/utils/validateHtml.ts
@@ -0,0 +1,43 @@
+import * as cheerio from "cheerio";
+
+/**
+ * Find closest number, higher than needle, from an array
+ * @param {number} needle
+ * @param {number[]} haystack
+ * @returns {number}
+ */
+ const closest = (needle: number, haystack: number[]): number => {
+  return haystack.length < 1
+    ? haystack.at(-1)! // we like bliding ej, Array.prototype.at is only available from 16.6.0
+    : haystack.reduce((curr, acc) => {
+      const aDiff = Math.abs(curr - needle);
+      const bDiff = Math.abs(acc - needle);
+
+      return bDiff < aDiff ? acc : curr;
+    }, 0);
+};
+
+/**
+ * Trim HTML according to tag
+ * @param {number} max
+ * @param {string} content
+ * @returns {string}
+ */
+export const trimHtml = (max: number, content: string): string => {
+  // add wrapper div for cheerio.children selector to work
+  // we want to select all tags _without_ their children
+  const $ = cheerio.load(`<div>${content}</div>`, {
+    xml: {
+      withStartIndices: true,
+      withEndIndices: true
+    }
+  });
+
+  const endIndices = $("div")
+    .children()
+    .get()
+    .map((el) => el.endIndex! + 1);
+
+  // -5 because the first `<div>` tag counts but we don't actually use them
+  return content.substring(0, closest(max, endIndices) - 5);
+};
