4b2c0e71499248a7dc572c2930dc93be52601989
elianiva <dicha.arkana03@gmail.com>
2022-06-28 11:34:51 +0700

    feat: attach github event handlers

--- src/infrastructure/server/GithubServer.ts
+++ src/infrastructure/server/GithubServer.ts
@@ -10,12 +10,32 @@ export class GithubServer implements IServer {
 
   public register(ctx: Context) {
     this._polka.use(this._config.path, this.verifySignature.bind(this));
-    this._polka.post(this._config.path, this.parsePayload.bind(this));
+    this._polka.post(this._config.path, this.handleWebhook.bind(this));
 
+    // handle issues events
     this._config.webhook.on("issue.opened", this._config.handlers.issues.opened(ctx));
+    this._config.webhook.on("issue.closed", this._config.handlers.issues.closed(ctx));
+    this._config.webhook.on("issue.edited", this._config.handlers.issues.edited(ctx));
+    this._config.webhook.on("issue.reopened", this._config.handlers.issues.reopened(ctx));
+    this._config.webhook.on("issue_comment.created", this._config.handlers.issues.commentCreated(ctx));
+    this._config.webhook.on("issue_comment.edited", this._config.handlers.issues.commentEdited(ctx));
+
+    // handle pull requests events
+    this._config.webhook.on("pull_request.opened", this._config.handlers.pr.opened(ctx));
+    this._config.webhook.on("pull_request.closed", this._config.handlers.pr.closed(ctx));
+    this._config.webhook.on("pull_request.edited", this._config.handlers.pr.edited(ctx));
+
+    // handle reviews events
+    this._config.webhook.on("pull_request_review.submitted", this._config.handlers.review.submitted(ctx));
+    this._config.webhook.on("pull_request_review_comment.created", this._config.handlers.review.created(ctx));
+    this._config.webhook.on("pull_request_review.edited", this._config.handlers.review.edited(ctx));
+
+    // handle release events
+    this._config.webhook.on("release.published", this._config.handlers.release.published(ctx));
+    this._config.webhook.on("deployment_status", this._config.handlers.deployment.status(ctx));
   }
 
-  private parsePayload(req: Request, res: Response) {
+  private handleWebhook(req: Request, res: Response) {
     const eventName = `${req.body.event}.${req.body.payload.action}` as WebhookEventName;
     this._config.webhook.handle(eventName, req.body.payload);
     // TODO: implementation
